76e7ac2de76c23a4357d22dc8924fc63c6320f58
Abhinish Kumar <141950751+Abhinish-Kumar@users.noreply.github.com>
2025-05-11 22:55:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://abhinish-kumar.github.io/instaFrontend",
+    origin: "https://abhinish-kumar.github.io",
     credentials: true,
   })
 );
